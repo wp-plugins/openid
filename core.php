df89de5c68295b0508be537f2db96cad0e4a7f1a
wnorris <wnorris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-11-10 01:52:38 +0000

    update descriptions and names for 2.0 release
    git-svn-id: https://plugins.svn.wordpress.org/openid/trunk@24422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core.php
+++ core.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: WordPress OpenID
-Plugin URI: http://willnorris.com/projects/wp-openid/
-Description: WordPress OpenID Registration, Authentication, and Commenting.
+Plugin Name: OpenID
+Plugin URI: http://wordpress.org/extend/plugins/
+Description: Allows the use of OpenID for account registration, authentication, and commenting.  <em>By <a href="http://verselogic.net">Alan Castonguay</a>.</em>
 Author: Will Norris
 Author URI: http://willnorris.com/
 Version: trunk
